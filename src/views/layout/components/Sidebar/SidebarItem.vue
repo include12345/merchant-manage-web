ea5285f63e1c8cd6faef7dde5f5e1291cd20b556
lihebin <lihebin@shouqianba.com>
2019-04-25 23:13:25 +0800

    布局

---
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <div v-for="item in routes" v-if="!item.hidden&&item.children">
+        <router-link v-if="hasOneShowingChildren(item.children) && !item.children[0].children && !item.alwaysShow" :to="item.path+'/'+item.children[0].path"
+        :key="item.children[0].name">
+
+        </router-link>
+
+    </div>
+</template>
