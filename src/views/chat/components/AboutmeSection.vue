86ae8f8a0795266bb7eddd48caf7e5606e75402e
lihebin <lihebin@shouqianba.com>
2020-08-16 21:46:26 +0800

    图片不实现了

--- src/views/chat/components/AboutmeSection.vue
+++ src/views/chat/components/AboutmeSection.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="aboutme-section">
-        <mt-cell class="mt-20 me" is-link to="/profile" :title="realName" :label="'chat ID:' +username">
-
-        </mt-cell>
-        <mt-cell class="mt-20" is-link :title="朋友圈" @click.native="comingSoon">
+        <div style="width: 100%;height:40px;"></div>
+            <mt-cell class="mt-20" style="min-height:60px;" :label= "tokens.username" :title="tokens.username" isLink to="/updateMe">
+                <img slot="icon" :src="tokens.imageUrl" width="50" height="50"/>
+                <div slot="title" class="info-title">
+                    <p>{{tokens.username}}</p>
+                    <p>chat ID: {{tokens.username}}</p>
+                </div>
+            </mt-cell>
+        <!-- <mt-cell class="mt-20 me"  :title="tokens.username" :label="'chat ID:' +tokens.username">
+            <img slot="icon" :src="tokens.imageUrl" width="24" height="24">
+        </mt-cell> -->
+        <!-- <mt-cell class="mt-20" is-link :title="朋友圈" @click.native="comingSoon">
             <span slot="icon" class="cell-icon moments icon icon-camera"></span>
         </mt-cell>
         <mt-cell is-link :title="附近朋友" to="/NearbyPeople">
             <span slot="icon" class="cell-icon near-by icon icon-man-woman"></span>
-        </mt-cell>
+        </mt-cell> -->
         <mt-cell class="mt-30" is-link to="/settings" :title="设置">
             <span slot="icon" class="cell-icon settings icon icon-cog"></span>
         </mt-cell>
@@ -16,6 +24,7 @@
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import UploadImages from '@/components/Upload/uploadImages'
 import {Toast} from 'mint-ui'
 export default {
     name: 'aboutme-section',
@@ -26,9 +35,7 @@ export default {
     },
     computed: {
         ...mapGetters([
-            'username',
-            'realName',
-            'profilePhotoVersion'
+            'tokens'
         ])
     },
     methods: {
@@ -40,6 +47,14 @@ export default {
 </script>
 <style lang="scss">
     .aboutme-section {
+        .info-title {
+            display: inline-block;
+            vertical-align: middle;
+            margin-left: 10px;
+            > p {
+                margin: 0;
+            }
+        }
         .me {
             // height: 70px;
             .mint-cell-wrapper {
@@ -53,7 +68,6 @@ export default {
                 .mint-cell-title {
                     position: absolute;
                     left: 70px;
-                    top: 15px;
                     .mint-cell-label {
                         color: #000;
                         font-size: 14px;
